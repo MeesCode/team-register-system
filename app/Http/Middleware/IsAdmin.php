e55cecb127866a522585ff32167f958f4b57bb2b
Mees <m.brinkhuis@student.tudelft.nl>
2019-09-30 17:18:27 +0200

    fix admin middleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Middleware;
 use Closure;
+use Illuminate\Support\Facades\Auth;
+
 class IsAdmin
 {
     /**
@@ -12,9 +14,9 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->isAdmin()) {
+        if(Auth::user() && Auth::user()->isAdmin()) {
             return $next($request);
         }
-        return redirect('home');
+        return redirect(route('home'));
     }
 }
